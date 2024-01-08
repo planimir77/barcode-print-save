db5f1a4b31819482896de3ef682af4ad9c778bbd
planimir77 <planimir7708@gmail.com>
2024-01-08 12:46:11 +0200

    refactor: implement possible add many barcodes

--- main.js
+++ main.js
@@ -1,33 +1,36 @@
-const printSaveButton = document.getElementsByClassName("print-save-button")[0];
+const title = document.getElementsByClassName("title")[0];
+const spiner = document.getElementsByClassName("spiner")[0];
+const barcode = document.getElementsByClassName("barcode")[0];
 const barcodes = document.getElementsByClassName("barcodes")[0];
-const barcodesRepeatData = document.getElementsByClassName("barcodes-repeat")[0];
-const inputNumberOfBarcodes = document.getElementsByClassName("number-of-barcodes")[0];
+const printSaveButton = document.getElementsByClassName("print-save-button")[0];
+const barcodesRepeatData =
+  document.getElementsByClassName("barcodes-repeat")[0];
+const inputNumberOfBarcodes =
+  document.getElementsByClassName("number-of-barcodes")[0];
 const inputBarcodeData = document.getElementsByClassName("barcode-data")[0];
 const inputSecondBarcodeData = document.getElementsByClassName(
   "second-barcode-data"
 )[0];
+const inputBarcodeSize = document.getElementsByClassName(
+  "slider-barcode-size"
+)[0];
+const inputProductName = document.getElementsByClassName("product-name")[0];
 
 const sizeDefaultValue = 5;
-const inputBarcodeSize = document.getElementsByClassName("slider-barcode-size")[0];
 inputBarcodeSize.value = sizeDefaultValue;
 
 const secondBarcode = document.getElementsByClassName("second-barcode")[0];
-const inputProductName = document.getElementsByClassName("product-name")[0];
 const sizeValue = document.getElementsByClassName("size-value")[0];
 let moduleWidth = 0;
 
 const barcodeProductName = document.getElementsByClassName(
   "barcode-product-name"
 )[0];
-const barcodeImage = document.getElementsByClassName("barcode-image")[0];
-const secondBarcodeImage = document.getElementsByClassName(
-  "second-barcode-image"
-)[0];
+
 function delay(time) {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
 function getModuleWidth(barcodeLength, barcodeSize) {
-
   const factor = barcodeSize > 1.3 ? 0.86 : 0.98;
 
   if (barcodeLength === 0 || barcodeLength === 1) {
@@ -40,46 +43,81 @@ const printSaveButtonClick = async () => {
   if (!inputBarcodeData.value) {
     alert("Please enter the barcode data");
   } else {
-    window.print();
+    spiner.classList.add("d-block");
+    await updateTitle();
+    await updateBarcodeData();
+    await updateSecondBarcodeData();
+    await updateBarcodesRepeatData();
+    setTimeout(() => {
+      spiner.classList.remove("d-block");
+      window.print();
+      // Clear data
+      barcode.innerText = "";
+      secondBarcode.innerHTML = "";
+      barcodesRepeatData.innerHTML = "";
+
+      updateTitle("Barcode Generator");
+
+    }, 2000);
   }
 };
-const updateBarcodeData = () => {
-  moduleWidth = getModuleWidth(
-    inputBarcodeData.value.length,
-    // Custom barcode size
-    Number(inputBarcodeSize.value)
-  );
-
+const updateTitle = async (value) => {
+  title.innerText = value
+    ? value
+    : new Date()
+        .toISOString()
+        .split(".")[0]
+        .replace("T", "_v")
+        .replaceAll(":", "");
+};
+const updateBarcodeData = async () => {
   if (!inputProductName.value) {
     barcodeProductName.style.marginTop = 0 + "px";
   }
 
   if (inputBarcodeData.value) {
-    const src = `https://barcode.tec-it.com/barcode.ashx?data=${inputBarcodeData.value}&translate-esc=on&unit=Mm&imagetype=Svg&moduleWidth=${moduleWidth}&font=Calibri%2C26%2Cbold`;
-    barcodeImage.src = src;
+    const barcodes = inputBarcodeData.value.split("\n");
+
+    for (const barcodeData of barcodes) {
+      if (!barcodeData) continue;
+      moduleWidth = getModuleWidth(
+        barcodeData.length,
+        // Custom barcode size
+        Number(inputBarcodeSize.value)
+      );
+
+      const image = document.createElement("img");
+      image.src = `https://barcode.tec-it.com/barcode.ashx?data=${barcodeData}&translate-esc=on&unit=Mm&imagetype=Svg&moduleWidth=${moduleWidth}&font=Calibri%2C26%2Cbold`;
+      image.alt = `Barcode ${barcodeData}`;
+      image.style.paddingBlock = "8px";
+      if (barcodeData.includes("IP-TMP-", 0)) {
+        image.classList.add("border-bottom");
+      }
+      barcode.appendChild(image);
+    }
   }
-  updateBarcodesRepeatData();
 };
 
-const updateSecondBarcodeData = () => {
+const updateSecondBarcodeData = async () => {
   moduleWidth = getModuleWidth(
     inputSecondBarcodeData.value.length,
     // Custom barcode size
     Number(inputBarcodeSize.value)
   );
 
-  if (!inputSecondBarcodeData.value || inputSecondBarcodeData.value == 'IP-TMP-') {
-    secondBarcode.classList.remove("border-bottom");
-    secondBarcode.classList.add("d-none");
-    inputNumberOfBarcodes.value = 1;
-    secondBarcodeImage.src = "";
-  } else {
-    secondBarcode.classList.add("border-bottom");
-    secondBarcode.classList.remove("d-none");
-    inputNumberOfBarcodes.value = 4;
-    secondBarcodeImage.src = `https://barcode.tec-it.com/barcode.ashx?data=${inputSecondBarcodeData.value}&translate-esc=on&unit=Mm&imagetype=Svg&modulewidth=${moduleWidth}&font=Calibri%2C26%2Cbold`;
+  if (
+    inputSecondBarcodeData.value &&
+    inputSecondBarcodeData.value.includes("IP-TMP-") &&
+    inputSecondBarcodeData.value.length > ("IP-TMP-").length
+  ) {
+    // Valid barcode data
+    const image = document.createElement("img");
+    image.src = `https://barcode.tec-it.com/barcode.ashx?data=${inputSecondBarcodeData.value}&translate-esc=on&unit=Mm&imagetype=Svg&moduleWidth=${moduleWidth}&font=Calibri%2C26%2Cbold`;
+    image.alt = `Barcode ${inputSecondBarcodeData.value}`;
+    image.style.paddingBlock = "8px";
+    image.classList.add("border-bottom");
+    secondBarcode.appendChild(image);
   }
-  updateBarcodesRepeatData();
 };
 
 const addProductName = () => {
@@ -88,31 +126,29 @@ const addProductName = () => {
 };
 const updateSizeValue = () => {
   if (sizeDefaultValue - Number(inputBarcodeSize.value) == 0) {
-    sizeValue.innerText = 'default'
+    sizeValue.innerText = "default";
   } else {
-    sizeValue.innerText = `${(sizeDefaultValue - Number(inputBarcodeSize.value)).toPrecision(2)}`
+    sizeValue.innerText = `${(
+      sizeDefaultValue - Number(inputBarcodeSize.value)
+    ).toPrecision(2)}`;
   }
 };
-const updateBarcodesRepeatData = ()=>{
-  // Clear data
-  barcodesRepeatData.innerHTML = "";
+const updateBarcodesRepeatData = async () => {
   // Update data
-  for (let index = 0; index < Number(inputNumberOfBarcodes.value) - 1; index++) {
+  for (
+    let index = 0;
+    index < Number(inputNumberOfBarcodes.value) - 1;
+    index++
+  ) {
     let node = barcodes.cloneNode(true);
     barcodesRepeatData.appendChild(node);
   }
-
-}
+};
 const barcodeSizeChange = () => {
   updateSizeValue();
-  updateBarcodeData();
-  updateSecondBarcodeData();
 };
 
-inputBarcodeData.addEventListener("change", updateBarcodeData);
-inputSecondBarcodeData.addEventListener("change", updateSecondBarcodeData);
 inputProductName.addEventListener("change", addProductName);
 inputBarcodeSize.addEventListener("change", barcodeSizeChange);
-inputNumberOfBarcodes.addEventListener("change", updateBarcodesRepeatData)
+inputNumberOfBarcodes.addEventListener("change", updateBarcodesRepeatData);
 printSaveButton.addEventListener("click", printSaveButtonClick);
-
